009170a3334fd1082c44ce7d7316108b0d6fbac7
mhorton-dev <michael@mhorton.dev>
2025-10-06 17:53:07 -0500

    Created .env file

--- src/api/useQuery.jsx
+++ src/api/useQuery.jsx
@@ -2,58 +2,72 @@ import { useState, useEffect } from "react";
 import { useApi } from "./ApiContext.jsx";
 
 /*
-API wrapper for retruning data, taggle loading status, error messages
+API wrapper for returning data, toggle loading status, error messages
 */
 
 export default function useQuery(resource, tag) {
-  const { request, provideTag } = useApi();
+  try {
+    const { request, provideTag } = useApi();
 
-  const [data, setDate] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+    const [data, setDate] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const query = async () => {
-    setLoading(true);
-    setError(null);
+    const query = async () => {
+      setLoading(true);
+      setError(null);
 
+      try {
+        const result = await request(resource);
+        setDate(result);
+      } catch (e) {
+        console.error(e);
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    useEffect(() => {
+      provideTag(tag, query);
+      query();
+    }, []);
+
+    return { data, loading, error };
+
+    const register = async (credentials) => {
+      const response = await fetch(API + "/users/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      });
+      const result = await response.json();
+      if (!response.ok) throw result;
+      setToken(result.token);
+    };
+
+    const login = () => {
+      setToken(null);
+      sessionStorage.removeItem("token");
+    };
+
+    const result = response.json();
     try {
-      const result = await request(resource);
-      setDate(result);
-    } catch (e) {
-      console.error(e);
-      setError(e.message);
-    } finally {
-      setLoading(false);
+      if (!response.ok) throw result;
+      setToken(result.token);
+    } catch (err) {
+      console.error(`AuthProvider error: ${err}`);
+
+      const logout = () => {
+        setToken(null);
+        sessionStorage.removeItem("token");
+
+        const value = { token, register, login, logout };
+        return;
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+      };
     }
-  };
-
-  useEffect(() => {
-    provideTag(tag, query);
-    query();
-  }, []);
-
-  return { data, loading, error };
-
-  const register = async (credintials) => {
-    const response = await fetch(API + "/users/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(credintials),
-    });
-    const result = await response.json();
-    if (!response.ok) throw result;
-    setToken(result.token);
-  };
-
-  const login = async (credentials) => {
-    const response = await fetch(API + "/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.sttingly(credentials),
-    });
-
-    const result = await response.json();
-    if (!response.ok) throw result;
-    setToken(result.token);
-  };
+  } catch (err) {
+    console.error(`useQuery error:  ${err}`);
+  }
 }
--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -11,19 +11,42 @@ export function AuthProvider({ children }) {
   }, [token]);
 
   const register = async (credentials) => {
-    const response = async fetch(API + "users/register", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(credentials),
+    const response = await fetch(API + "users/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
     });
     const result = await response.json();
     if (!response.ok) throw result;
     setToken(result.token);
-  }
+  };
 
-  const login = async(credentials) => {
-    const response = await fetch (API + users/login, {
-        method "POST"
-    })
+  const login = async (credentials) => {
+    const response = await fetch(API + "users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    });
+
+    const result = await response.json();
+    if (!response.ok) throw result;
+    setToken(result.token);
+  };
+
+  const logout = () => {
+    setToken(null);
+    sessionStorage.removeItem("token");
+  };
+
+  const value = { token, register, login, logout };
+  return <AuthContext.Provider value={value}>{children} </AuthContext.Provider>;
+}
+
+export function useAuth() {
+  try {
+    const context = useContext(AuthContext);
+    if (!context) throw Error(`useAuth context) ${Error}`);
+  } catch (err) {
+    console.error(`useAuth threw ${err}`);
   }
 }
